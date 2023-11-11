5dc42d2b46aca6ed7e029e45d5840c21bf2e9dfe
deepanshu5628 <d2810201@gmail.com>
2023-11-12 04:05:54 +0530

    added router.route functionality

--- routes/listing.js
+++ routes/listing.js
@@ -7,28 +7,39 @@ const { islogedin, isowner } = require("../middleware");
 
 const listingcontroller = require("../controller/listing");
 
+// new button
+router.get("/new", islogedin, listingcontroller.newbtnroute)
+
+router.route("/")
+    .get(wrapasync(listingcontroller.indexroute))
+    .post(islogedin, wrapasync(listingcontroller.postroute));
+
+router.route("/:id")
+    .get(islogedin, isowner, wrapasync(listingcontroller.editbtnroute))
+    .patch(islogedin, isowner, wrapasync(listingcontroller.updateroute))
+    .delete(islogedin, isowner, wrapasync(listingcontroller.destroyroute));
+
 
 // Index route
-router.get("/", wrapasync(listingcontroller.indexroute));
+// router.get("/", wrapasync(listingcontroller.indexroute));
 
 // show route
 router.get("/show/:id", wrapasync(listingcontroller.showroute));
 
-// new button
-router.get("/new", islogedin, listingcontroller.newbtnroute)
+
 
 // create Route
-router.post("/", islogedin, wrapasync(listingcontroller.postroute));
+// router.post("/", islogedin, wrapasync(listingcontroller.postroute));
 
 
 // edit button 
-router.get("/:id", islogedin, isowner, wrapasync(listingcontroller.editbtnroute));
+// router.get("/:id", islogedin, isowner, wrapasync(listingcontroller.editbtnroute));
 
 // update route
-router.patch("/:id", islogedin, isowner, wrapasync(listingcontroller.updateroute));
+// router.patch("/:id", islogedin, isowner, wrapasync(listingcontroller.updateroute));
 
 
 // Delete route
-router.delete("/:id", islogedin, isowner, wrapasync(listingcontroller.destroyroute));
+// router.delete("/:id", islogedin, isowner, wrapasync(listingcontroller.destroyroute));
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -6,17 +6,27 @@ const passport = require("passport");
 const { savedurl } = require("../middleware");
 const controller = require("../controller/user");
 
-// api for signup 
-router.get("/signup", controller.sigupbtn)
 
-router.post("/signup", wrapasync(controller.signuppost));
+// router.route
+router.route("/signup")
+    .get(controller.sigupbtn)
+    .post(wrapasync(controller.signuppost));
+
+const authentication = passport.authenticate("local", { failureRedirect: "/login", failureFlash: true });
+router.route("/login")
+    .get(controller.loginbtn)
+    .post(savedurl, authentication, controller.loginroute);
+
+// api for signup 
+// router.get("/signup", controller.sigupbtn)
+// 
+// router.post("/signup", wrapasync(controller.signuppost));
 
 
 // api's for login
-router.get("/login", controller.loginbtn);
-
-const authentication = passport.authenticate("local", { failureRedirect: "/login", failureFlash: true })
-router.post("/login", savedurl, authentication, controller.loginroute)
+// router.get("/login", controller.loginbtn);
+// 
+// router.post("/login", savedurl, authentication, controller.loginroute)
 
 // api for logout 
 router.get("/logout", controller.logoutbtn)
